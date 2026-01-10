10dd3778b20fb018a5557ea2264ee4d849bb72bb
Bart-15 <brat.tabusao@gmail.com>
2026-01-10 22:54:16 +0800

    feat: ✨ fix imports and enhance the logic

--- app/src/main/java/com/bart/apipractice/network/ApiService.kt
+++ app/src/main/java/com/bart/apipractice/network/ApiService.kt
@@ -1,2 +1,9 @@
 package com.bart.apipractice.network
 
+import com.bart.apipractice.model.User
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("users")
+    suspend fun getUsers(): List<User>
+}
--- app/src/main/java/com/bart/apipractice/network/RetrofitInstance.kt
+++ app/src/main/java/com/bart/apipractice/network/RetrofitInstance.kt
@@ -1,6 +1,5 @@
 package com.bart.apipractice.network
 
-import com.google.firebase.appdistribution.gradle.ApiService
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import kotlin.jvm.java
@@ -8,7 +7,7 @@ import kotlin.jvm.java
 object RetrofitInstance {
     private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
-    private val apiService: ApiService by lazy {
+    val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
