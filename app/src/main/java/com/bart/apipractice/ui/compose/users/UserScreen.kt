984ab1b99a1e1957e65b7c4ccb289fe1fc7ae981
Bart-15 <brat.tabusao@gmail.com>
2026-01-11 14:49:57 +0800

    refactor: ♻️ implement the AppNavHost

--- app/src/main/java/com/bart/apipractice/ui/compose/users/UserScreen.kt
+++ app/src/main/java/com/bart/apipractice/ui/compose/users/UserScreen.kt
@@ -3,15 +3,22 @@ package com.bart.apipractice.ui.compose.users
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bart.apipractice.repository.UserRepository
@@ -58,10 +65,23 @@ fun UserScreen(
             val users = (uiState as UserUiState.Success).users
 
             LazyColumn(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier.fillMaxSize()
+                    .statusBarsPadding(),
                 contentPadding = PaddingValues(vertical = 8.dp)
             ) {
 
+                item {
+                    Text(
+                        text = "User List",
+                        modifier = Modifier.padding(8.dp),
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+
+                items(users) { user ->
+                    UserCard(user = user, onClick = onUserClick)
+                }
             }
         }
     }
