a6d37698a12b434f04f170e250b10e3c0f9afdfa
Bart-15 <brat.tabusao@gmail.com>
2026-01-11 16:43:27 +0800

    refactor: ♻️ add user detail screen

--- app/src/main/java/com/bart/apipractice/navigation/AppNavHost.kt
+++ app/src/main/java/com/bart/apipractice/navigation/AppNavHost.kt
@@ -1,9 +1,12 @@
 package com.bart.apipractice.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.bart.apipractice.ui.compose.users.UserDetailScreen
 import com.bart.apipractice.ui.compose.users.UserScreen
 
 @Composable
@@ -17,6 +20,18 @@ fun AppNavHost() {
                 navController.navigate("${NavRoutes.USER_DETAIL}/$userId")
             })
         }
+
+        composable(
+            route = "${NavRoutes.USER_DETAIL}/{userId}",
+            arguments = listOf(navArgument("userId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val userId = backStackEntry.arguments?.getInt("userId") ?: 0
+            UserDetailScreen(
+                userId = userId,
+                onBackClick = { navController.popBackStack() } // <--- Use popBackStack
+            )
+        }
+
     }
 
 }
