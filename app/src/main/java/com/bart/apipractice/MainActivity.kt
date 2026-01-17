460ad32f6f96b3cb109695f1c01abc8adadfbe8c
Bart-15 <brat.tabusao@gmail.com>
2026-01-17 16:25:52 +0800

    feat: ✨ setup hilt

--- app/src/main/java/com/bart/apipractice/MainActivity.kt
+++ app/src/main/java/com/bart/apipractice/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.bart.apipractice.navigation.AppNavHost
 import com.bart.apipractice.ui.theme.APIPracticeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
