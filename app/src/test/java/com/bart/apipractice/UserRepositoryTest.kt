e947c7354b8c0d1eb216d1f60f8029cecf28b9fc
Bart-15 <brat.tabusao@gmail.com>
2026-01-18 16:29:48 +0800

    chore: 💬 add FakeUsers data

--- app/src/test/java/com/bart/apipractice/UserRepositoryTest.kt
+++ app/src/test/java/com/bart/apipractice/UserRepositoryTest.kt
@@ -1,11 +1,8 @@
 package com.bart.apipractice
 
-import com.bart.apipractice.model.Address
-import com.bart.apipractice.model.Company
-import com.bart.apipractice.model.Geo
-import com.bart.apipractice.model.User
 import com.bart.apipractice.network.ApiService
 import com.bart.apipractice.repository.UserRepository
+import com.bart.apipractice.testdata.FAKE_USERS
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
@@ -14,50 +11,6 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.jupiter.api.Assertions.assertEquals
 import kotlin.test.Test
-
-val FAKE_USERS = listOf(
-    User(
-        id = 1,
-        name = "John Doe",
-        userName = "johnd",
-        email = "john@example.com",
-        address = Address(
-            street = "123 Main St",
-            suite = "Apt 1",
-            city = "Metropolis",
-            zipcode = "12345",
-            geo = Geo(lat = "12.3456", lng = "65.4321")
-        ),
-        phone = "123-456-7890",
-        website = "johndoe.com",
-        company = Company(
-            name = "Doe Industries",
-            catchPhrase = "Innovate Everything",
-            bs = "business solutions"
-        )
-    ),
-    User(
-        id = 2,
-        name = "Jane Smith",
-        userName = "janes",
-        email = "jane@example.com",
-        address = Address(
-            street = "456 Side St",
-            suite = "Suite 200",
-            city = "Gotham",
-            zipcode = "67890",
-            geo = Geo(lat = "98.7654", lng = "43.2109")
-        ),
-        phone = "987-654-3210",
-        website = "janesmith.io",
-        company = Company(
-            name = "Smith Co",
-            catchPhrase = "Think Different",
-            bs = "tech solutions"
-        )
-    )
-)
-
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class UserRepositoryTest {
